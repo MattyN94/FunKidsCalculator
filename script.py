36c8844a7340b061146d7636439f1df90e7acf2e
Matt Napier <matt.napier4@gmail.com>
2024-03-21 12:58:22 +0000

    Add files via upload

---
+++ script.py
@@ -0,0 +1,43 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "ERemember, you can't divide by 0. Please try again!"
+    else:
+        return x / y
+
+while True:
+    print("Let's get calculating!")
+    print("Choose what you'd like to do:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Thanks for playing! Goodbye!")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("That's not a number we're looking for sorry, please try again.")
